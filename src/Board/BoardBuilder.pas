d460830a9e84b20ee1d0f847b89acae77042faf7
João Victor <joao22t@gmail.com>
2025-07-13 00:19:50 -0300

    create render board routine

--- src/Board/BoardBuilder.pas
+++ src/Board/BoardBuilder.pas
@@ -4,9 +4,11 @@ interface
 
 uses
   System.Classes, System.Types, System.SysUtils, Vcl.ExtCtrls,
-  Vcl.Controls;
+  Vcl.Controls, BoardState, BoardPiece, ImageLoader;
 
 type
+  TBoardMatrix = array[0..7, 0..7] of TPanel;
+
   TRowBuilder = class
   public
     class function Build(const BoardPanel: TPanel): TPanel; static;
@@ -22,18 +24,38 @@ type
     class procedure Build(const SquarePanel: TPanel); static;
   end;
 
+  IBoard = interface
+  ['{9E74D5D8-3789-4B7F-A288-EEED76F9F589}']
+    procedure SetState(const State: IBoardState);
+    procedure SetBoardPanel(const BoardPanel: TPanel);
+    procedure SetBoardMatrix(const BoardMatrix: TBoardMatrix);
+    procedure Render;
+  end;
+
+  TBoard = class(TInterfacedObject, IBoard)
+  private
+    FState: IBoardState;
+    FBoardPanel: TPanel;
+    FBoardMatrix: TBoardMatrix;
+  public
+    procedure SetState(const State: IBoardState);
+    procedure SetBoardPanel(const BoardPanel: TPanel);
+    procedure SetBoardMatrix(const BoardMatrix: TBoardMatrix);
+    procedure Render;
+  end;
+
   IBoardBuilder = interface
   ['{C15DF4E4-4B03-4C73-B7DD-560151028BC9}']
-    function Board(BoardPanel: TPanel): IBoardBuilder;
-    function Build: IBoardBuilder;
+    function Board(const BoardPanel: TPanel): IBoardBuilder;
+    function Build(State: IBoardState): IBoard;
   end;
 
   TBoardBuilder = class(TInterfacedObject, IBoardBuilder)
   private
     FBoardPanel: TPanel;
   public
-    function Board(BoardPanel: TPanel): IBoardBuilder;
-    function Build: IBoardBuilder;
+    function Board(const BoardPanel: TPanel): IBoardBuilder;
+    function Build(State: IBoardState): IBoard;
   end;
 
 implementation
@@ -75,28 +97,85 @@ begin
   SquareImage.Parent := SquarePanel;
   SquareImage.Align := alClient;
   SquareImage.Cursor := crHandPoint;
+  SquareImage.Proportional := True;
+end;
+
+{ TBoard }
+
+procedure TBoard.SetState(const State: IBoardState);
+begin
+  FState := State;
+end;
+
+procedure TBoard.SetBoardPanel(const BoardPanel: TPanel);
+begin
+  FBoardPanel := BoardPanel;
+end;
+
+procedure TBoard.SetBoardMatrix(const BoardMatrix: TBoardMatrix);
+begin
+  FBoardMatrix := BoardMatrix;
+end;
+
+procedure TBoard.Render;
+var
+  Piece: IPiece;
+  PieceName: string;
+  PiecePanel: TPanel;
+  Row, Col: Integer;
+  SquareImage: TImage;
+  PieceMatrix: TPieceMatrix;
+begin
+  PieceMatrix := FState.GetPieceMatrix();
+
+  for Row := 0 to 7 do
+  begin
+    for Col := 0 to 7 do
+    begin
+      Piece := PieceMatrix[Row, Col];
+
+      if not Assigned(Piece) then
+        Continue;
+
+      PiecePanel := FBoardMatrix[Row, Col];
+
+      //TODO: Make a helper
+      PieceName := TObject(Piece).ClassName.Remove(0, 1);
+
+      SquareImage := TImage(PiecePanel.Components[0]);
+      TImageLoader.Load(PieceName, SquareImage);
+    end;
+  end;
+
 end;
 
 { TBoardBuilder }
 
-function TBoardBuilder.Board(BoardPanel: TPanel): IBoardBuilder;
+function TBoardBuilder.Board(const BoardPanel: TPanel): IBoardBuilder;
 begin
   FBoardPanel := BoardPanel;
   Result := Self;
 end;
 
-function TBoardBuilder.Build: IBoardBuilder;
+function TBoardBuilder.Build(State: IBoardState): IBoard;
 var
-  I, J: Integer;
+  Row, Col: Integer;
   RowPanel: TPanel;
+  BoardMatrix: TBoardMatrix;
 begin
-  for I := 1 to ROWS do
+  Result := TBoard.Create();
+
+  for Row := 0 to Pred(ROWS) do
   begin
     RowPanel := TRowBuilder.Build(FBoardPanel);
 
-    for J := 1 to COLUMNS do
-      TSquareBuilder.Build(RowPanel);
+    for Col := 0 to Pred(COLUMNS) do
+      BoardMatrix[Row, Col]:= TSquareBuilder.Build(RowPanel);
   end;
+
+  Result.SetState(State);
+  Result.SetBoardPanel(FBoardPanel);
+  Result.SetBoardMatrix(BoardMatrix);
 end;
 
 end.
