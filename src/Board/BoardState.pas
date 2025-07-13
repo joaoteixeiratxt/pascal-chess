e79bd235abbed1167cdf77a08b29fb954c89cad8
João Victor <joao22t@gmail.com>
2025-07-13 00:19:10 -0300

    adjust state to use TPieceMatrix

--- src/Board/BoardState.pas
+++ src/Board/BoardState.pas
@@ -6,9 +6,12 @@ uses
   System.Classes, System.Types, BoardPiece;
 
 type
+  TPieceMatrix = array[0..7, 0..7] of IPiece;
+
   IBoardState = interface
   ['{984AB04C-D208-47A0-8A1C-71634201AB3B}']
     procedure Initialize;
+    function GetPieceMatrix: TPieceMatrix;
     function GetPieceAt(const Row, Col: Integer): IPiece;
     procedure SetPieceAt(const Row, Col: Integer; const Value: IPiece);
     procedure MovePiece(const FromRow, FromCol, ToRow, ToCol: Integer);
@@ -16,9 +19,10 @@ type
 
   TBoardState = class(TInterfacedObject, IBoardState)
   private
-    FState: array[0..7, 0..7] of IPiece;
+    FBoardMatrix: TPieceMatrix;
   public
     procedure Initialize;
+    function GetPieceMatrix: TPieceMatrix;
     function GetPieceAt(const Row, Col: Integer): IPiece;
     procedure SetPieceAt(const Row, Col: Integer; const Value: IPiece);
     procedure MovePiece(const FromRow, FromCol, ToRow, ToCol: Integer);
@@ -34,47 +38,52 @@ var
 begin
   for Row := 0 to 7 do
     for Column := 0 to 7 do
-      FState[Row, Column] := nil;
+      FBoardMatrix[Row, Column] := nil;
 
-  FState[7, 0] := TPieceFactory.New(ptRook);
-  FState[7, 1] := TPieceFactory.New(ptKnight);
-  FState[7, 2] := TPieceFactory.New(ptBishop);
-  FState[7, 3] := TPieceFactory.New(ptQueen);
-  FState[7, 4] := TPieceFactory.New(ptKing);
-  FState[7, 5] := TPieceFactory.New(ptBishop);
-  FState[7, 6] := TPieceFactory.New(ptKnight);
-  FState[7, 7] := TPieceFactory.New(ptRook);
-
-  for Column := 0 to 7 do
-    FState[6, Column] := TPieceFactory.New(ptPawn);
-
-  FState[0, 0] := TPieceFactory.New(ptRook);
-  FState[0, 1] := TPieceFactory.New(ptKnight);
-  FState[0, 2] := TPieceFactory.New(ptBishop);
-  FState[0, 3] := TPieceFactory.New(ptQueen);
-  FState[0, 4] := TPieceFactory.New(ptKing);
-  FState[0, 5] := TPieceFactory.New(ptBishop);
-  FState[0, 6] := TPieceFactory.New(ptKnight);
-  FState[0, 7] := TPieceFactory.New(ptRook);
+  FBoardMatrix[7, 0] := TPieceFactory.New(ptRook);
+  FBoardMatrix[7, 1] := TPieceFactory.New(ptKnight);
+  FBoardMatrix[7, 2] := TPieceFactory.New(ptBishop);
+  FBoardMatrix[7, 3] := TPieceFactory.New(ptQueen);
+  FBoardMatrix[7, 4] := TPieceFactory.New(ptKing);
+  FBoardMatrix[7, 5] := TPieceFactory.New(ptBishop);
+  FBoardMatrix[7, 6] := TPieceFactory.New(ptKnight);
+  FBoardMatrix[7, 7] := TPieceFactory.New(ptRook);
 
   for Column := 0 to 7 do
-    FState[1, Column] := TPieceFactory.New(ptPawn);
+    FBoardMatrix[6, Column] := TPieceFactory.New(ptPawn);
+
+  FBoardMatrix[0, 0] := TPieceFactory.New(ptRook);
+  FBoardMatrix[0, 1] := TPieceFactory.New(ptKnight);
+  FBoardMatrix[0, 2] := TPieceFactory.New(ptBishop);
+  FBoardMatrix[0, 3] := TPieceFactory.New(ptQueen);
+  FBoardMatrix[0, 4] := TPieceFactory.New(ptKing);
+  FBoardMatrix[0, 5] := TPieceFactory.New(ptBishop);
+  FBoardMatrix[0, 6] := TPieceFactory.New(ptKnight);
+  FBoardMatrix[0, 7] := TPieceFactory.New(ptRook);
+
+  for Column := 0 to 7 do
+    FBoardMatrix[1, Column] := TPieceFactory.New(ptPawn);
+end;
+
+function TBoardState.GetPieceMatrix: TPieceMatrix;
+begin
+  Result := FBoardMatrix;
 end;
 
 function TBoardState.GetPieceAt(const Row, Col: Integer): IPiece;
 begin
-  Result := FState[Row, Col];
+  Result := FBoardMatrix[Row, Col];
 end;
 
 procedure TBoardState.SetPieceAt(const Row, Col: Integer; const Value: IPiece);
 begin
-  FState[Row, Col] := Value;
+  FBoardMatrix[Row, Col] := Value;
 end;
 
 procedure TBoardState.MovePiece(const FromRow, FromCol, ToRow, ToCol: Integer);
 begin
-  FState[ToRow, ToCol] := FState[FromRow, FromCol];
-  FState[FromRow, FromCol] := nil;
+  FBoardMatrix[ToRow, ToCol] := FBoardMatrix[FromRow, FromCol];
+  FBoardMatrix[FromRow, FromCol] := nil;
 end;
 
 end.
