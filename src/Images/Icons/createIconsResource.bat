be4b1a33a20c60fbbd8d2dcb896042b0171a24e6
João Victor <joao22t@gmail.com>
2025-08-09 20:48:05 -0300

    add LoadGIF util

--- src/Images/Icons/createIconsResource.bat
+++ src/Images/Icons/createIconsResource.bat
@@ -6,7 +6,7 @@ set "RES_FILE=Icons.res"
 if exist "%RC_FILE%" del "%RC_FILE%"
 if exist "%RES_FILE%" del "%RES_FILE%"
 
-for %%F in (*.png) do (
+for %%F in (*.png, *.gif) do (
     set "NAME=%%~nF"
     echo !NAME! RCDATA "%%F" >> "%RC_FILE%"
 )
--- src/Images/ImageLoader.pas
+++ src/Images/ImageLoader.pas
@@ -3,12 +3,14 @@ unit ImageLoader;
 interface
 
 uses
-  System.Classes, System.Types, Vcl.ExtCtrls, Vcl.Imaging.pngimage;
+  System.Classes, System.Types, Vcl.ExtCtrls, Vcl.Imaging.pngimage,
+  Vcl.Imaging.GIFImg;
 
 type
   TImageLoader = class
   public
     class procedure Load(ImageName: string; Image: TImage);
+    class procedure LoadGIF(GifName: string; Image: TImage);
   end;
 
 implementation
@@ -30,6 +32,27 @@ begin
       Image.Picture.Graphic := Png;
     finally
       Png.Free;
+    end;
+  finally
+    ResStream.Free;
+  end;
+end;
+
+class procedure TImageLoader.LoadGIF(GifName: string; Image: TImage);
+var
+  GIF: TGIFImage;
+  ResStream: TResourceStream;
+begin
+  ResStream := TResourceStream.Create(HInstance, GifName, RT_RCDATA);
+  try
+    GIF := TGIFImage.Create;
+    try
+      GIF.LoadFromStream(ResStream);
+      GIF.Animate := True;
+      GIF.AnimationSpeed := 100;
+      Image.Picture.Assign(GIF);
+    finally
+      GIF.Free;
     end;
   finally
     ResStream.Free;
